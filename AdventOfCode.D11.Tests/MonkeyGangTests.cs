defd7ecf86b83f475983a90f741d865560ea3a4c
Piotrowski, Maciej <maciej.piotrowski@accenture.com>
2022-12-12 15:12:17 +0100

    Solution for D11.

---
+++ AdventOfCode.D11.Tests/MonkeyGangTests.cs
@@ -0,0 +1,284 @@
+﻿using System.Numerics;
+using static AdventOfCode.D11.Program;
+
+namespace AdventOfCode.D11.Tests
+{
+    public class MonkeyGangTests
+    {
+        [Fact]
+        public void MonkeyGangInitTest()
+        {
+            var gang = new MonkeyGang();
+            var operation = new Func<BigInteger, BigInteger>(x => x + 1);
+
+            gang.AddMonkey(new Monkey(id: 1,
+                                      new Queue<BigInteger>(),
+                                      operation,
+                                      test: 1,
+                                      passIfDivisable: 1,
+                                      passIfNotDivisable: 2));
+
+            Assert.Contains(gang.Monkeys, m => m.Id == 1);
+        }
+
+        [Fact]
+        public void ThrowTest()
+        {
+            var input =
+@"Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1";
+
+            var gang = new MonkeyGang(input);
+
+            Assert.Equal(4, gang.Monkeys.Count);
+
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+
+            // After round 1.
+            Assert.Equal(new List<BigInteger> { 20, 23, 27, 26 }, gang.Monkeys.First().Items);
+            Assert.Equal(new List<BigInteger> { 2080, 25, 167, 207, 401, 1046 }, gang.Monkeys.Skip(1).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(2).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(3).First().Items);
+
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+
+            // After round 2.
+            Assert.Equal(5, gang.Monkeys.First().Items.Count);
+            Assert.Equal(5, gang.Monkeys.Skip(1).First().Items.Count);
+            Assert.Empty(gang.Monkeys.Skip(2).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(3).First().Items);
+
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+
+            // After round 3.
+            Assert.Equal(5, gang.Monkeys.First().Items.Count);
+            Assert.Equal(5, gang.Monkeys.Skip(1).First().Items.Count);
+            Assert.Empty(gang.Monkeys.Skip(2).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(3).First().Items);
+
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+            gang.NextMonkeyTurn();
+
+            // After round 4.
+            Assert.Equal(new List<BigInteger> { 491, 9, 52, 97, 248, 34 }, gang.Monkeys.First().Items);
+            Assert.Equal(new List<BigInteger> { 39, 45, 43, 258 }, gang.Monkeys.Skip(1).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(2).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(3).First().Items);
+        }
+
+        [Fact]
+        public void RoundTest()
+        {
+            var input =
+@"Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1";
+
+            var gang = new MonkeyGang(input);
+
+            Assert.Equal(4, gang.Monkeys.Count);
+
+            gang.Round();
+
+            // After round 1.
+            Assert.Equal(new List<BigInteger> { 20, 23, 27, 26 }, gang.Monkeys.First().Items);
+            Assert.Equal(new List<BigInteger> { 2080, 25, 167, 207, 401, 1046 }, gang.Monkeys.Skip(1).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(2).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(3).First().Items);
+
+            gang.Round();
+
+            // After round 2.
+            Assert.Equal(5, gang.Monkeys.First().Items.Count);
+            Assert.Equal(5, gang.Monkeys.Skip(1).First().Items.Count);
+            Assert.Empty(gang.Monkeys.Skip(2).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(3).First().Items);
+
+            gang.Round();
+
+            // After round 3.
+            Assert.Equal(5, gang.Monkeys.First().Items.Count);
+            Assert.Equal(5, gang.Monkeys.Skip(1).First().Items.Count);
+            Assert.Empty(gang.Monkeys.Skip(2).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(3).First().Items);
+
+            gang.Round();
+
+            // After round 4.
+            Assert.Equal(new List<BigInteger> { 491, 9, 52, 97, 248, 34 }, gang.Monkeys.First().Items);
+            Assert.Equal(new List<BigInteger> { 39, 45, 43, 258 }, gang.Monkeys.Skip(1).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(2).First().Items);
+            Assert.Empty(gang.Monkeys.Skip(3).First().Items);
+        }
+
+        [Fact]
+        public void RoundComplexTest()
+        {
+            var input =
+@"Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1";
+
+            var gang = new MonkeyGang(input);
+
+            for (BigInteger i = 0; i < 20; i++)
+            {
+                gang.Round();
+            }
+
+            Assert.Equal(101u, gang.Monkeys.First().InspectedItemsCount);
+            Assert.Equal(95u, gang.Monkeys.Skip(1).First().InspectedItemsCount);
+            Assert.Equal(7u, gang.Monkeys.Skip(2).First().InspectedItemsCount);
+            Assert.Equal(105u, gang.Monkeys.Skip(3).First().InspectedItemsCount);
+        }
+
+        [Fact]
+        public void ThrowAdjustedTest()
+        {
+            var input =
+@"Monkey 0:
+  Starting items: 79, 98
+  Operation: new = old * 19
+  Test: divisible by 23
+    If true: throw to monkey 2
+    If false: throw to monkey 3
+
+Monkey 1:
+  Starting items: 54, 65, 75, 74
+  Operation: new = old + 6
+  Test: divisible by 19
+    If true: throw to monkey 2
+    If false: throw to monkey 0
+
+Monkey 2:
+  Starting items: 79, 60, 97
+  Operation: new = old * old
+  Test: divisible by 13
+    If true: throw to monkey 1
+    If false: throw to monkey 3
+
+Monkey 3:
+  Starting items: 74
+  Operation: new = old + 3
+  Test: divisible by 17
+    If true: throw to monkey 0
+    If false: throw to monkey 1";
+
+            var gang = new MonkeyGang(input);
+            //gang.RoundAdjusted();
+
+            //Assert.Equal(2, gang.Monkeys.First().InspectedItemsCount);
+            //Assert.Equal(4, gang.Monkeys.Skip(1).First().InspectedItemsCount);
+            //Assert.Equal(3, gang.Monkeys.Skip(2).First().InspectedItemsCount);
+            //Assert.Equal(6, gang.Monkeys.Skip(3).First().InspectedItemsCount);
+
+            //for (BigInteger i = 0; i < 19; i++)
+            //{
+            //    gang.RoundAdjusted();
+            //}
+
+            //Assert.Equal(99, gang.Monkeys.First().InspectedItemsCount);
+            //Assert.Equal(97, gang.Monkeys.Skip(1).First().InspectedItemsCount);
+            //Assert.Equal(8, gang.Monkeys.Skip(2).First().InspectedItemsCount);
+            //Assert.Equal(103, gang.Monkeys.Skip(3).First().InspectedItemsCount);
+
+            for (int i = 0; i < 10000; i++)
+            {
+                gang.RoundAdjusted();
+            }
+
+            Assert.Equal(52166, gang.Monkeys.First().InspectedItemsCount);
+            Assert.Equal(47830, gang.Monkeys.Skip(1).First().InspectedItemsCount);
+            Assert.Equal(1938, gang.Monkeys.Skip(2).First().InspectedItemsCount);
+            Assert.Equal(52013, gang.Monkeys.Skip(3).First().InspectedItemsCount);
+
+            var top2 = gang.Monkeys.OrderByDescending(m => m.InspectedItemsCount).Take(2);
+            Assert.Equal(2713310158, top2.First().InspectedItemsCount * top2.Skip(1).First().InspectedItemsCount);
+
+        }
+    }
+}
---
+++ AdventOfCode.D11/Program.cs
@@ -0,0 +1,302 @@
+﻿using System.Numerics;
+
+namespace AdventOfCode.D11
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Part1();
+            Part2();
+        }
+
+        private static void Part1()
+        {
+            Console.WriteLine("What is the level of monkey business "
+                + "after 20 rounds of stuff-slinging simian shenanigans?");
+
+            var input = File.ReadAllText("input.txt");
+            var gang = new MonkeyGang(input);
+
+            for (BigInteger i = 0; i < 20; i++)
+            {
+                gang.Round();
+            }
+
+            var ordered = gang.Monkeys.OrderByDescending(m => m.InspectedItemsCount);
+            var first = ordered.First();
+            var second = ordered.Skip(1).First();
+
+            var result = first.InspectedItemsCount * second.InspectedItemsCount;
+            Console.WriteLine(result);
+        }
+
+        public class Monkey
+        {
+            public BigInteger Id { get; private set; }
+            public Queue<BigInteger> Items { get; init; }
+            public Func<BigInteger, BigInteger> Operation { get; init; }
+            public int Test { get; init; }
+            public int PassIfDivisable { get; init; }
+            public int PassIfNotDivisable { get; init; }
+            public BigInteger InspectedItemsCount { get; private set; } = 0;
+
+            private string _sign = string.Empty;
+            private string _value = string.Empty;
+
+            public Monkey(BigInteger id,
+                          Queue<BigInteger> items,
+                          Func<BigInteger, BigInteger> operation,
+                          int test,
+                          int passIfDivisable,
+                          int passIfNotDivisable)
+            {
+                Id = id;
+                Items = items ?? throw new ArgumentNullException(nameof(items));
+                Operation = operation ?? throw new ArgumentNullException(nameof(operation));
+                Test = test;
+                PassIfDivisable = passIfDivisable;
+                PassIfNotDivisable = passIfNotDivisable;
+            }
+
+            public Monkey(string input)
+            {
+                string[] split = input.Split(Environment.NewLine);
+
+                var rawId = split[0]
+                    .Split(" ")[1]
+                    .Replace(":", string.Empty);
+
+                Id = BigInteger.Parse(rawId);
+
+                var rawItems = split[1]
+                    .Replace("  Starting items: ", string.Empty)
+                    .Split(",");
+
+                Items = new Queue<BigInteger>(rawItems.Select(i => BigInteger.Parse(i)));
+
+                _sign = split[2].Split(" ")[6];
+                _value = split[2].Split(" ")[7];
+
+                if (_sign == "*" && _value != "old")
+                {
+                    Operation = (BigInteger x) => x * BigInteger.Parse(_value);
+                    //Operation = (BigInteger x) => BigInteger.Multiply(x, BigInteger.Parse(_value));
+                    //Operation = (BigInteger x) => PowerOptimised(x, 2);
+                    //Operation = (BigInteger x) => BigInteger.Parse(Karatsuba.Multiply(x.ToString(), _value));
+
+                }
+                else if (_sign == "+" && _value != "old")
+                {
+                    Operation = (BigInteger x) => x + BigInteger.Parse(_value);
+                }
+                else if (_sign == "+" && _value == "old")
+                {
+                    Operation = (BigInteger x) => x + x;
+                }
+                else
+                {
+                    Operation = (BigInteger x) => x * x;
+                    //Operation = (BigInteger x) => x << 1;
+                    //Operation = (BigInteger x) => PowerOptimised(x, 2);
+                    //Operation = (BigInteger x) => BigInteger.Parse(Karatsuba.Multiply(x.ToString(), x.ToString()));
+
+                }
+
+                Test = int.Parse(split[3].Split(" ").Last());
+                PassIfDivisable = int.Parse(split[4].Split(" ").Last());
+                PassIfNotDivisable = int.Parse(split[5].Split(" ").Last());
+            }
+
+            public List<int> Inspect()
+            {
+                return Items.Select(
+                    i =>
+                    {
+                        BigInteger v = Operation(i) / 3;
+                        return v % Test == 0 ? PassIfDivisable
+                                             : PassIfNotDivisable;
+                    })
+                    .ToList();
+            }
+
+            public List<(int, BigInteger)> InspectAdjusted(List<Monkey> monkeys)
+            {
+                var output = new List<(int, BigInteger)>();
+
+                foreach (var item in Items)
+                {
+                    var newWorry = Process(item);
+                    int aggregate = monkeys.Aggregate(1, (i, monkey) => i * monkey.Test);
+                    newWorry %= aggregate;
+
+                    BigInteger worryLevel = newWorry % Test;
+                    var targetMonkey = worryLevel == 0
+                                        ? PassIfDivisable
+                                        : PassIfNotDivisable;
+
+                    output.Add((targetMonkey, newWorry));
+                }
+
+                return output;
+
+
+                //return Items.Select(
+                //    i =>
+                //    {
+                //        //BigInteger v = Operation(i);
+                //        BigInteger v = Process(i);
+                //        v %= monkeys.Aggregate(1, (i, monkey) => i * monkey.Test);
+
+                //        BigInteger worryLevel = v % Test;
+                //        return worryLevel == 0 ? PassIfDivisable
+                //                             : PassIfNotDivisable;
+                //    })
+                //    .ToList();
+            }
+
+            private BigInteger Process(BigInteger i)
+            {
+                if (_sign == "*" && _value != "old")
+                {
+                    return (BigInteger)i * BigInteger.Parse(_value);
+                }
+                else if (_sign == "+" && _value != "old")
+                {
+                    return i + BigInteger.Parse(_value);
+                }
+                else if (_sign == "+" && _value == "old")
+                {
+                    return i + i;
+                }
+                else
+                {
+                    return (BigInteger)i * (BigInteger)i;
+                }
+            }
+
+            public void Throw(Monkey target)
+            {
+                var item = Items.Dequeue();
+                item = Operation(item) / 3;
+                target.Items.Enqueue(item);
+
+                InspectedItemsCount++;
+            }
+
+            public void ThrowAdjusted(Monkey target, BigInteger value)
+            {
+                _ = Items.Dequeue();
+                // item = Operation(item);
+                target.Items.Enqueue(value);
+
+                InspectedItemsCount++;
+            }
+        }
+
+        public class MonkeyGang
+        {
+            private readonly List<Monkey> _monkeys = new();
+            private BigInteger _currentMonkeyIndex = 0;
+            private BigInteger _maxMonkeyIndex = -1;
+            public IReadOnlyList<Monkey> Monkeys => _monkeys;
+
+            public MonkeyGang()
+            {
+
+            }
+
+            public MonkeyGang(string raw)
+            {
+                foreach (string section in raw.Split("\r\n\r\n"))
+                {
+                    AddMonkey(new Monkey(section));
+                }
+                _maxMonkeyIndex = _monkeys.Count - 1;
+            }
+
+            public void AddMonkey(Monkey monkey)
+            {
+                _monkeys.Add(monkey);
+                _maxMonkeyIndex++;
+            }
+
+            public void NextMonkeyTurn()
+            {
+                if (_currentMonkeyIndex > _maxMonkeyIndex)
+                {
+                    _currentMonkeyIndex = 0;
+                }
+
+                var monkey = _monkeys[(int)_currentMonkeyIndex];
+                var throwPlan = monkey.Inspect();
+
+                foreach (BigInteger index in throwPlan)
+                {
+                    var throwTo = _monkeys[(int)index];
+                    monkey.Throw(throwTo);
+                }
+
+                _currentMonkeyIndex++;
+            }
+
+            public void Round()
+            {
+                for (int i = 0; i < _monkeys.Count; i++)
+                {
+                    NextMonkeyTurn();
+                }
+            }
+
+            public void RoundAdjusted()
+            {
+                for (int i = 0; i < _monkeys.Count; i++)
+                {
+                    NextMonkeyTurnAdjusted();
+                }
+            }
+
+            public void NextMonkeyTurnAdjusted()
+            {
+                if (_currentMonkeyIndex > _maxMonkeyIndex)
+                {
+                    _currentMonkeyIndex = 0;
+                }
+
+                var monkey = _monkeys[(int)_currentMonkeyIndex];
+
+
+                var throwPlan = monkey.InspectAdjusted(_monkeys);
+
+                foreach ((int index, BigInteger value) in throwPlan)
+                {
+                    var throwTo = _monkeys[(int)index];
+                    monkey.ThrowAdjusted(throwTo, value);
+                }
+
+                _currentMonkeyIndex++;
+            }
+        }
+
+        private static void Part2()
+        {
+            Console.WriteLine("Starting again from the initial state in your puzzle input, "
+                + "what is the level of monkey business after 10000 rounds?");
+
+            var input = File.ReadAllText("input.txt");
+            var gang = new MonkeyGang(input);
+
+            for (BigInteger i = 0; i < 10000; i++)
+            {
+                gang.RoundAdjusted();
+            }
+
+            var ordered = gang.Monkeys.OrderByDescending(m => m.InspectedItemsCount);
+            var first = ordered.First();
+            var second = ordered.Skip(1).First();
+
+            var result = first.InspectedItemsCount * second.InspectedItemsCount;
+            Console.WriteLine(result);
+        }
+    }
+}
