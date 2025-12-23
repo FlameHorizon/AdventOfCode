f030d5423d7b0dfd9283a5112793d9f750de0daa
FlameHorizon <maciejpiotrowsk@gmail.com>
2025-12-23 12:48:14 +0100

    Solution for D2 2025

--- 2025/D2.Tests/UnitTest1.cs
+++ 2025/D2.Tests/UnitTest1.cs
@@ -14,14 +14,28 @@ public class UnitTest1
     }
 
     [Theory]
-    [InlineData("11-22", 2, 33)]
-    [InlineData("95-115", 1, 99)]
-    [InlineData("1188511880-1188511890", 1, 1188511885)]
-    [InlineData("222220-222224", 1, 222222)]
-    [InlineData("1698522-1698528", 0, 0)]
-    public void CountsInvalidIds(string line, int count, int sum) {
+    [InlineData("11-22", 2)]
+    [InlineData("95-115", 2)]
+    [InlineData("1188511880-1188511890", 1)]
+    [InlineData("222220-222224", 1)]
+    [InlineData("1698522-1698528", 0)]
+    public void CountsInvalidIds(string line, int count) {
       IdRange result = Parser.Parse(line);
       Assert.Equal(count, result.CountOfInvalidIds());
-      Assert.Equal(sum, result.InvalidIds().Sum());
+    }
+
+    [Fact]
+    public void ReturnsInvalidIds() {
+      List<long> result = Parser.Parse("95-115").InvalidIds();
+      Assert.Equal((99, 111), (result.First(), result.Last()));
+
+      result = Parser.Parse("565653-565659").InvalidIds();
+      Assert.Equal(565656, result.First());
+
+      result = Parser.Parse("824824821-824824827").InvalidIds();
+      Assert.Equal(824824824, result.First());
+
+      result = Parser.Parse("2121212118-2121212124").InvalidIds();
+      Assert.Equal(2121212121, result.First());
     }
 }
--- 2025/D2/Program.cs
+++ 2025/D2/Program.cs
@@ -33,8 +33,6 @@ public static class Parser
     public static IdRange Parse(string line)
     {
         string[] split = line.Split(_idSeparator);
-        Console.WriteLine($"Start: {split[0]}");
-        Console.WriteLine($"End: {split[1]}");
         return new IdRange()
         {
             Start = Convert.ToInt64(split[0]),
@@ -63,6 +61,21 @@ public class IdRange
         for (long i = Start; i <= End; i++)
         {
             string number = i.ToString();
+
+            if (number.Length == 1)
+            {
+                Console.WriteLine("Number contians only one digit.");
+                continue;
+            }
+
+            // Case where all digits are the same.
+            if (number.Distinct().Count() == 1)
+            {
+                Console.WriteLine("All digits are the same in {0}", number);
+                result.Add(i);
+                continue;
+            }
+
             if (number.Length % 2 == 0)
             {
                 int middle = number.Length / 2;
@@ -70,7 +83,57 @@ public class IdRange
                 string right = number[middle..];
                 if (left == right)
                 {
+                    Console.WriteLine("Found palindrome, left-right {0} - {1} in {2}", left, right, number);
                     result.Add(i);
+                    continue;
+                }
+            }
+
+            // Depending on the size of the number
+            // repeating pattern will have either 2 or 3 characters.
+            // If size is even, pattern must repeating pattern
+            // must have length of two. Otherwise, pattern must have 
+            // length of three.
+            List<int> possiblePhraseLength = [];
+
+            if (number.Length % 6 == 0)
+            {
+                // Length of pattern can be both 3 or 2.
+                // Console.WriteLine("For number '{0}', selected 3 and 2 length.", number);
+                possiblePhraseLength.Add(3);
+                possiblePhraseLength.Add(2);
+            }
+            else if (number.Length % 3 == 0)
+            {
+                // Console.WriteLine("For number '{0}', selected 3 length.", number);
+                possiblePhraseLength.Add(3);
+            }
+            else
+            {
+                // Console.WriteLine("For number '{0}', selected 2 length.", number);
+                possiblePhraseLength.Add(2);
+            }
+
+            foreach (int length in possiblePhraseLength)
+            {
+                // Console.WriteLine("Using length: {0}", length);
+                string phrase = number[0..length];
+                // Console.WriteLine("Got phrase: {0}", phrase);
+                int occurenceCount = 0;
+                int index = 0;
+                while ((index = number.IndexOf(phrase, index)) != -1)
+                {
+                    // Console.WriteLine("At index {0}", index);
+                    index += length;
+                    occurenceCount++;
+                }
+
+                if (occurenceCount > 1 &&
+                    occurenceCount * length == number.Length)
+                {
+                    Console.WriteLine("Found repeating phrase '{0}' in {1}", phrase, number);
+                    result.Add(i);
+                    continue;
                 }
             }
         }
